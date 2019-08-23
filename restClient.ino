2704bac41300da526850caa14fbffbd57150e851
Tobias <tobf@web.de>
2019-08-23 18:00:01 +0200

    Update restClient.ino

--- restClient.ino
+++ restClient.ino
@@ -1,5 +1,4 @@
 #include <UIPEthernet.h>
-#include <LiquidCrystal_I2C.h>
 
 /*
   Rest
@@ -16,7 +15,6 @@
 byte mac[] = { MACBYTE, MACBYTE, MACBYTE, MACBYTE, MACBYTE, MACBYTE };
 IPAddress ip(OKT1, OKT2, OKT3, 242);
 EthernetServer server(80);
-LiquidCrystal_I2C lcd(0x3F, 20, 4);
 
 String readString = String(20);
 
@@ -25,17 +23,13 @@ void setup() {
 
   pinMode(4, OUTPUT);
   pinMode(5, INPUT);
-  lcd.begin();
-  lcd.clear();
-  lcd.print("W");
   sendaMsg(false);
 
   server.begin();
-  lcd.print("F");
 }
 
 void sendaMsg(boolean bUseBackupAddr) {
-  lcd.print("I");
+
   EthernetClient client;
   IPAddress IP;
 
@@ -49,10 +43,8 @@ void sendaMsg(boolean bUseBackupAddr) {
     
     while ((client.available()) < 0) {
       delay(10);
-      lcd.print(".");
     }
-    lcd.print("P");
-    
+ 
     client.println(("GET /wipf/s HTTP/1.1\r\nHost: 0.0.0.0:8080\r\n\r\n"));
     //delay(1000);
     client.stop();
@@ -80,13 +72,10 @@ void loop() {
           client.println();
 
           client.print("{");
-          client.print(digitalRead(5));
           client.println("}");
 
           // Nur PUT zulassen
           if (readString.indexOf("PUT /") > -1) {
-
-
             if (readString.indexOf("doOn")  > -1) {
               digitalWrite(4, HIGH);
               //client.print("ON");
@@ -95,39 +84,9 @@ void loop() {
               digitalWrite(4, LOW);
               //client.print("OFF");
             }
-            else if (readString.indexOf("cls")  > -1) {
-              lcd.clear();
-              lcd.setCursor(0, 0);
-            }
-            else if (readString.indexOf("/!~") == 4) {
-              // Jede Position ansteuern mit z.B. /!~103
-              int col = (readString.substring(7, 9).toInt());
-              int row = readString.substring(9, 10).toInt();
-
-              lcd.setCursor(col, row);
-
-            }
-
-            else {
-              String ss = readString;
-              ss.replace("%20", " ");
-
-              // GET /xxx HTTP/1.1
-              //      xxx
-              String s = ss.substring(ss.indexOf("/") + 1, ss.lastIndexOf("HTTP/1.1") - 1);
-              s.remove(s.length());
-              if (s.length() <= 20) {
-                lcd.print(s);
-              } else {
-                lcd.print("E2");
-              }
-            }
           }
-          else {
-            lcd.print("E1");
-          }
+
           readString = "";
-
           //stopping client
           client.stop();
         }
@@ -135,8 +94,6 @@ void loop() {
     }
   }
 }
-
-
           //stopping client
           client.stop();
         }
